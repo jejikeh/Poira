75fffe8185e958157ba67c3ccc12f55dd7e01688
jejikeh <jejikeh@gmail.com>
2023-03-18 19:20:02 +0300

    update ctor of product model

--- Core/Poira.Domain/Models/Product.cs
+++ Core/Poira.Domain/Models/Product.cs
@@ -2,12 +2,12 @@
 
 namespace Poira.Domain.Models;
 
-public class Products : EntityGuid
+public class Product : EntityGuid
 {
     public string Name { get; set; }
     public int DefaultQuantity { get; set; }
 
-    public Products(Guid id, string name, int defaultQuantity) : base(id)
+    public Product(Guid id, string name, int defaultQuantity) : base(id)
     {
         Name = name;
         DefaultQuantity = defaultQuantity;
